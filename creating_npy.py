eea84a90359867b91c33fe34073f80a6c3ba0c2c
Safwen Naimi <safwen.naimi@gmail.com>
2024-11-23 14:51:10 -0500

    Add files via upload

---
+++ creating_npy.py
@@ -0,0 +1,31 @@
+import os
+import json
+import numpy as np
+
+main_folder_path = 'E:/stm/Actions_keypoints/'
+output_folder_path = 'E:/stm/Actions_keypoints/'
+
+for root, dirs, files in os.walk(main_folder_path):
+    for folder_name in dirs:
+        folder_path = os.path.join(root, folder_name)
+        output_file_path = os.path.join(output_folder_path, folder_name)
+        
+        # Initialize an empty list to store the data
+        all_data = []
+
+        # Iterate over each file in the folder
+        for filename in os.listdir(folder_path):
+            if filename.endswith('.json'):
+                file_path = os.path.join(folder_path, filename)
+                with open(file_path, 'r') as f:
+                    # Load JSON data from the file
+                    data = json.load(f)
+                    # Append the data to the list
+                    all_data.append(data)
+
+        # Convert the list of data into a numpy array
+        concatenated_data = np.concatenate(all_data)
+
+        # Save the data with the folder name
+        np.save(output_file_path, concatenated_data)
+        print(f"Saved data for folder {folder_name} with shape {concatenated_data.shape}")
---
+++ generating_train_test.py
@@ -0,0 +1,151 @@
+import numpy as np
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+import os
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, LayerNormalization, Dropout
+from tensorflow.keras.callbacks import TensorBoard
+from tensorflow.keras.optimizers import Adam, SGD, Nadam, Adadelta, Adamax
+import wandb
+from wandb.keras import WandbCallback
+import tensorflow as tf
+
+#wandb.init(entity="safwennaimi", sync_tensorboard=True)
+
+def focal_loss(gamma=2.0, alpha=0.25):
+    def loss(y_true, y_pred):
+        epsilon = tf.keras.backend.epsilon()
+        y_true = tf.cast(y_true, tf.float32)  # Cast y_true to float32
+        y_pred = tf.clip_by_value(y_pred, epsilon, 1.0 - epsilon)
+        cross_entropy = -y_true * tf.math.log(y_pred)
+        focal_loss = alpha * tf.math.pow(1 - y_pred, gamma) * cross_entropy
+        return tf.reduce_sum(focal_loss, axis=-1)
+    return loss
+
+
+
+if __name__ == "__main__":
+    wandb.login()
+    wandb.init(entity="safwennaimi", project="ViTPose-LSTM_UCLA")
+    config = wandb.config
+    #config.lr = 1e-3 #1e-5
+    config.batch_size = 4 #4
+    config.epochs = 100
+    config.dropout = 0.1
+    config.recurrent_dropout = 0.1
+    config.filters_1 = 64
+    config.filters_2 = 128
+    config.lstm = 16
+    config.lr = 0.0009524
+    config.dense = 90
+    config.kernel = 14
+    config.pool = 12
+
+    crouch = np.load("E:/stm/Actions_keypoints/crouch+mcad.npy")
+    #crouch_mcad = np.load("E:/stm/Actions_keypoints/crouch+mcad.npy")
+    look_tunnel = np.load("E:/stm/Actions_keypoints/look_tunnel.npy")
+    sit_down = np.load("E:/stm/Actions_keypoints/sit_down.npy")
+    standing = np.load("E:/stm/Actions_keypoints/standing.npy")
+    UseCellPhone = np.load("E:/stm/Actions_keypoints/UseCellPhone.npy")
+    walk = np.load("E:/stm/Actions_keypoints/walk.npy")
+
+
+    crouch = np.array(crouch)
+    #crouch_mcad = np.array(crouch_mcad)
+    look_tunnel = np.array(look_tunnel)
+    sit_down = np.array(sit_down)
+    standing = np.array(standing)
+    UseCellPhone = np.array(UseCellPhone)
+    walk = np.array(walk)
+
+
+        # Reshape to (num_blocks, seq_len, num_keypoints, coords)
+    num_blocks_1 = crouch.shape[0]
+    seq_len_1 = crouch.shape[1]
+    num_keypoints = 25 #25
+    num_coords = 3
+    crouch = crouch.reshape(num_blocks_1, seq_len_1, num_keypoints, num_coords)
+    
+    num_blocks_2 = look_tunnel.shape[0]
+    seq_len_2 = look_tunnel.shape[1]
+    num_keypoints = 25 #25
+    num_coords = 3
+    look_tunnel = look_tunnel.reshape(num_blocks_2, seq_len_2, num_keypoints, num_coords)
+    
+    # Reshape to (num_blocks, seq_len, num_keypoints, coords)
+    num_blocks_3 = sit_down.shape[0]
+    seq_len_3 = sit_down.shape[1]
+    num_keypoints = 25 #25
+    num_coords = 3
+    sit_down = sit_down.reshape(num_blocks_3, seq_len_3, num_keypoints, num_coords)
+
+    # Reshape to (num_blocks, seq_len, num_keypoints, coords)
+    num_blocks_4 = standing.shape[0]
+    seq_len_4 = standing.shape[1]
+    num_keypoints = 25 #25
+    num_coords = 3
+    standing = standing.reshape(num_blocks_4, seq_len_4, num_keypoints, num_coords)
+
+    # Reshape to (num_blocks, seq_len, num_keypoints, coords)
+    num_blocks_5 = UseCellPhone.shape[0]
+    seq_len_5 = UseCellPhone.shape[1]
+    num_keypoints = 25  #25
+    num_coords = 3
+    UseCellPhone = UseCellPhone.reshape(num_blocks_5, seq_len_5, num_keypoints, num_coords)
+
+    # Reshape to (num_blocks, seq_len, num_keypoints, coords)
+    num_blocks_6 = walk.shape[0]
+    seq_len_6 = walk.shape[1]
+    num_keypoints = 25  #25
+    num_coords = 3
+    walk = walk.reshape(num_blocks_6, seq_len_6, num_keypoints, num_coords)
+
+    
+
+
+
+
+    Final = np.concatenate((crouch, look_tunnel, sit_down, standing, UseCellPhone, walk))
+    print(Final.shape)
+    #print(wave.shape)
+
+
+
+    crouch_label = np.zeros((698), dtype="int").reshape(-1,1)
+    look_tunnel_label = np.ones((139), dtype="int").reshape(-1,1)
+    sit_down_label = 2 * np.ones((1556), dtype="int").reshape(-1,1)
+    standing_label = 3 * np.ones((3030), dtype="int").reshape(-1,1)
+    UseCellPhone_label = 4 * np.ones((819), dtype="int").reshape(-1,1)
+    walk_label = 5 * np.ones((2974), dtype="int").reshape(-1,1)
+    #crouch_mcad_label = 6 * np.ones((698), dtype="int").reshape(-1,1)
+
+
+
+    labels = np.vstack([crouch_label, look_tunnel_label, sit_down_label, standing_label, UseCellPhone_label, walk_label])
+    encoder = OneHotEncoder()
+    y = encoder.fit_transform(labels).toarray().astype(int)
+    #y = encoded_labels.toarray()
+    num_classes = len(np.unique(y, axis=0))
+    new_y = []
+    for sample in y:
+        label = np.where(sample==1)[0][0]
+        new_y.append(label)
+        
+    y = np.array(new_y)
+    #print(new_y.shape)
+    #for y in new_y: 
+    #    print(y)
+    #print(y.shape)
+    
+    
+    X_train, X_test, y_train, y_test = train_test_split(Final, y, test_size = 0.20, shuffle=True)
+    print(X_train.shape)
+    print(X_test.shape)
+    print(y_train.shape)
+    print(y_test.shape)
+    
+    np.save('E:/stm/Actions_keypoints/X_train+mcad.npy', X_train)
+    np.save('E:/stm/Actions_keypoints/X_test+mcad.npy', X_test) 
+    np.save('E:/stm/Actions_keypoints/y_train+mcad.npy', y_train)
+    np.save('E:/stm/Actions_keypoints/y_test+mcad.npy', y_test)
+    
